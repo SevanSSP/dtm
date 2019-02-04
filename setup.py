242a2021ba25f8896dab82a0811490bcd8b82b0c
pev <pev@sevanssp.com>
2019-02-04 14:24:32 +0100

    Adds package setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    """Utility function to read the README file."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='dtm',
+    version='0.1.0',
+    packages=find_packages(exclude='tests'),
+    url='https://github.com/SevanSSP/dtm',
+    license='MIT',
+    author='Per Voie',
+    author_email='pev@sevanssp.com',
+    description='Distributed task manager.',
+    long_description=read('README.md'),
+    entry_points={
+        'console_scripts': [
+            'dtm-run=dtm.main:cli',
+        ],
+    }
+)
