576d0e91e441cd258c02fe0c7ca6aa036ed164a8
pev <pev@sevanssp.com>
2019-08-05 09:26:14 +0200

    Bumped the version patch number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='dtm',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(exclude='tests'),
     url='https://github.com/SevanSSP/dtm',
     license='MIT',
