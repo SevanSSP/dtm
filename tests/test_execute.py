5fb038f169eb5aef61a600eb884118d56f111ae0
pev <pev@sevanssp.com>
2019-08-05 09:24:59 +0200

    Created tests for `execute_task`.

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,9 +1,26 @@
 import unittest
+from dtm.main import execute_task
+
+# TODO: Test running a script (does not require shell). Unfortunately we have to create a temp. file on the worker.
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestCase(unittest.TestCase):
+    def test_commands_requiring_shell(self):
+        r = execute_task(('dir', '/s', '/b'), pipe=True)
+        self.assertEqual(r.get('returncode'), 1)
+        self.assertEqual(r.get('status'), 'error')
+
+        r = execute_task(('dir', '/s', '/b'), pipe=True, shell=True)
+        self.assertEqual(r.get('returncode'), 0)
+        self.assertEqual(r.get('status'), 'completed')
+
+        r = execute_task(('python --version', ), pipe=True)
+        self.assertEqual(r.get('returncode'), 1)
+        self.assertEqual(r.get('status'), 'error')
+
+        r = execute_task(('python --version',), pipe=True, shell=True)
+        self.assertEqual(r.get('returncode'), 0)
+        self.assertEqual(r.get('status'), 'completed')
 
 
 if __name__ == '__main__':
